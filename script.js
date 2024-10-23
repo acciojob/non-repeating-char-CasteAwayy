7bac7cf5f9e6951b95350cf7dbda1fe02343ac08
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-10-23 16:23:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count = 1;
+	let store = Array(26).fill(0);
+	for(let i=0;i<str.length;i++)
+		{
+			store[str[i].charCodeAt()-97]++;
+		}
+	for(let i=0;i<str.length;i++)
+		{if(store[str[i].charCodeAt()-97]==1) return str[i];}
+	
+	return null;
 }
+
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
